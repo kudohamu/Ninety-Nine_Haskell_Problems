85653718596d7b1ea2aa597d7c09c0c21c8765ab
kudohamu <kudohamu91086@yahoo.co.jp>
2014-08-25 19:35:32 +0900

    add comment

--- problem10.hs
+++ problem10.hs
@@ -7,6 +7,6 @@ encodeFactory::[[a]] -> [(Int, a)]
 encodeFactory [] = []
 encodeFactory list = (length $ head list, head $ head list):(encodeFactory $ tail list)
 
-
+--各要素に関数を適用する（要素を(a->b)したい）ならmapが便利
 encode'::(Eq a) => [a] -> [(Int, a)]
 encode' = map (\n -> (length n, head n)) . group
